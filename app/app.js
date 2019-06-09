b7ed8adbcafff950b423f5707eeb7bd53ef2b9eb
Luan Roger <luan.roger.2003@gmail.com>
2019-06-09 15:46:52 -0300

    V0.4 Random ~ Dev
    - Nome de branch modificada.
    - Corrigdo erros.
    - Potencia e Raiz quadrada adicionadas.
    - Modificações na estrutura do código.

--- app/app.js
+++ app/app.js
@@ -17,7 +17,7 @@ function Start() {
     // Boas-Vindas
     console.log("===========================================".gray);
     console.log("") //Separador
-    console.log("     Bem-vindo a calculadora.js v0.3".blue.bold);
+    console.log("     Bem-vindo a calculadora.js v0.4".blue.bold);
     console.log("") //Sepadador
     console.log("===========================================".gray);
     //---------------------------------------------------------
@@ -29,7 +29,7 @@ function Start() {
     //--------------------------------------------------------------------------
     //Condições menu
     if (menuSelect == 1) {
-        opetacoesBasicas();
+        operacoesBasicas();
     }
     else if (menuSelect == 2) {
         randomGerate();
@@ -40,12 +40,14 @@ function Start() {
     }
     //----------------------------------------------------------------
 
+
     function randomGerate(min, max, randomResult) {
         //Aviso
         console.log("ATENCAO: essa funcao esta em fase de teste\nPode haver alguns bugs.".red.bold);
         //-----------------------------------------------------------
         //Variaveis
-        min = readline.questionInt("Digite o valor minimo: ".yellow);
+        min = 0;
+        console.log("Valor minimo definido para: 0".yellow);
         max = readline.questionInt("Digite o valor maximo: ".yellow);
         randomResult = Math.floor(Math.random() * (max - min + 1) - min);
         //-------------------------------------------------------
@@ -61,55 +63,77 @@ function Start() {
 
         //Apresentar valor
         console.log("O valor gerado é: ".blue.bold + randomResult);
+        Continuar();
         //-------------------------------------
     }
 
+
     //Oerações basicas
-    function opetacoesBasicas() {
-            //Variaveis1
+    function operacoesBasicas() {
+
+        //Variaveis2
+        console.log("[ + ] Somar.     [ - ] Subtrair.\n[ * ] Multiplicar.     [ / ] Dividir.\n[ ! ] Potencia.     [ # ] Raiz quadrada.".cyan);
+        var operation = readline.question("Qual operacao voce que fazer?".yellow);
+        //-----------------------------------------------------------------------------------------------
+
+        //Evitar erros (Variaveis2)
+        if (operation == "+" || operation == "-" || operation == "*" || operation == "/" || operation == "!" || operation == "#") {
+            
+        }
+        else {
+            console.log("Voce digitou um valor invalido.".red);
+            operacoesBasicas();
+        }
+        //------------------------------------------------------------
+
+        console.log("================================================================================".gray);
+        
+        //Variaveis1
         var nun1 = readline.questionInt("Digite um numero: ".yellow);
         var nun2 = readline.questionInt("Digite outro numero: ".yellow);
         var result;
         //-----------------------------------------------------------
-
-        console.log("================================================================================".gray);
-
-        //Variaveis2
-        console.log("[ + ] Somar.     [ - ] Subtrair.\n[ * ] Multiplicar.     [ / ] Dividir.".cyan);
-        var operration = readline.question("Qual operacao voce que fazer?".yellow);
-        //-----------------------------------------------------------------------------------------------
-
-        // Condições/Manupulações
-        if (operration == "+") {
+        
+        // Condições/Manipulações
+        if (operation == "+") {
             result = nun1 + nun2;
         }
-        else if (operration == "-") {
+        else if (operation == "-") {
             result = nun1 - nun2;
         }
-        else if (operration == "*") {
+        else if (operation == "*") {
             result = nun1 * nun2;
         }
-        else if (operration == "/") {
+        else if (operation == "/") {
             result = nun1 / nun2;
         }
-        else {
+        else if (operation == "!") {
+            result = Math.pow(nun1, nun2);
+        }
+        else if (operation == "#") {
+            result = Math.sqrt(nun1);
+        }
+        else{
             console.log("Voce digitou um valor invalido.".red.bold);
-            Start();
+            operacoesBasicas();
         }
         //-----------------------------------------------------------------
-
+        
         //Apresentação do resultado final
         console.log("O resultado da operacao e: ".blue.bold + result);
+        Continuar();
         //----------------------------------------------------------------
 
     }
     //Continuar
-    if(readline.keyInYN("Deseja fazer outra operacao?".yellow)) {
-        //Caso Y
-        Start();
-    }
-    else {
-        process.exit()
+    function Continuar() {
+        if(readline.keyInYN("Deseja fazer outra operacao?".yellow)) {
+            //Caso Y
+            Start();
+        }
+        else {
+            process.exit()
+        }
     }
     //-----------------------------------------------------------------------
     
